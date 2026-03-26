8aa1404346efc560edea00910c7508c61361759a
Kory <200974544+Kory221@users.noreply.github.com>
2026-03-26 23:06:04 +0100

    all mergeSort tests passed

--- merge-sort.js
+++ merge-sort.js
@@ -2,29 +2,32 @@ export const mergeSort = (arr) => {
     if (arr.length <2) return arr;
 
     const result = [];
+    const mid = Math.ceil (arr.length/2)
 
-    if (arr.length % 2 ===0) {
-        const left = mergeSort(arr.slice(0, arr.length/2));
-        const right = mergeSort(arr.slice(arr.length/2));
-        
-        for (let i=0; i<left.length; i++) {
-            for (let j=0; j<right.length; j++) {
-                if (left[i] < right[j]) result.push(left[i]);
-                else result.push(right[j]);
-            }
-        }
-    }
-    else if (arr.length % 2 !==0) {
-        const left = mergeSort(arr.slice(0, (arr.length+1)/2));
-        const right = mergeSort(arr.slice((arr.length+1)/2));
+    
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
 
-        for (let i=0; i<left.length; i++) {
-            for (let j=0; j<right.length; j++) {
-                if (left[i] < right[j]) result.push(left[i]);
-                else result.push(right[j]);
-            }
+    let i = 0;
+    let j = 0;
+
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            result.push(left[i]);
+            i++;
         }
-        
+        else {
+            result.push(right[j]);
+            j++;
+        }
     };
+
+    if (i >= left.length) {
+        result.push(...right.filter((element) => !result.includes(element)));
+    }
+    else if (j>= right.length) {
+        result.push(...left.filter((element) => !result.includes(element)));
+    };
+    
     return result;
 };
